d1de2b4cf6b9c14d568665e75f35beaa9227f3e1
Fedor Shmarov <shmarovfedor@mail.ru>
2026-08-27 14:14:58 +0100

    [refactor] removed unused methods from the decision procedure and the smt2 generator

--- src/util/decision_procedure.h
+++ src/util/decision_procedure.h
@@ -62,17 +62,6 @@ int evaluate_complement(
   std::string,
   std::string);
 
-// first argument contains the odes to compute and the invariants to check
-// second argument is the time horizon for ode integration
-// third argument is the interval initial condition for the ode system
-int check_invariants(
-  pdrh::mode *,
-  capd::interval,
-  box,
-  std::vector<box>,
-  std::string,
-  std::string);
-
 } // namespace decision_procedure
 
 #endif //PROBREACH_DECISION_PROCEDURE_H
